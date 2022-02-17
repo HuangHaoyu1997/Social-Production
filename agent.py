24f2e6e8785e61d2ff79b2f2e699284b82b33efb
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-02-17 23:21:05 +0800

    update

---
+++ agent.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class agent:
+    def __init__(self,param) -> None:
+        
+        self.type = param.type
+        self.coin = param.init_coin
+        
---
+++ env.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class env:
+    def __init__(self,param) -> None:
+        self.agent_num = param.N
+        
+    def total_value(self,):
+        pass
+
+    def add_agent(self,):
+        pass
+
+def consume(agent):
+    # 消费，随机量m∈[0,m_a]
+    # agent的钱减少m，市场价值增加m
+    pass
+
+def salary(agent):
+    pass
