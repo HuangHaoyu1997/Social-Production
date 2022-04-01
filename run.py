87b8e48164e24edeec8cf6b5cd8e7ca29c85c4ec
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-04-01 10:44:07 +0800

    update

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ agent_coin = [[],[],[],[]]
 w = [[],[]]
 RSV = []
 RH = [0.] # RH for Rate of Hire
+JoblossRate = []
 data = []
 
 
@@ -44,6 +45,8 @@ for t in range(config.T):
     agent_num[1].append(len(env.E))
     agent_num[2].append(len(env.W))
     agent_num[3].append(alive_num(env.agent_pool))
+
+    JoblossRate.append(agent_num[0][-1] / agent_num[3][-1])
 
     agent_coin[0].append(avg_coin_u)
     agent_coin[1].append(avg_coin_e)
@@ -80,7 +83,7 @@ for t in range(config.T):
         ax6 = fig1.add_subplot(245) # Grid可视化
         ax7 = fig1.add_subplot(246) # Graph可视化
         ax8 = fig1.add_subplot(247) # 最低工资、最高工资变化趋势图
-        
+        ax9 = fig1.add_subplot(248) # 失业率
         
         ax1.cla()
         ax1.plot(agent_num[0])
@@ -134,7 +137,14 @@ for t in range(config.T):
         ax7.set_title('Top and bottom salary')
         ax7.legend(['bottom','top'],loc=2)
 
+        
+
         ax8.cla()
+        ax8.plot(JoblossRate)
+        ax8.grid(); ax8.set_xlabel('T'); ax8.set_ylabel('Jobless Rate')
+        ax8.set_title('Jobless Rate')
+
+        ax9.cla()
         p = nx.spring_layout(env.G)
         nx.draw(env.G, pos=p)
         # plt.show()
