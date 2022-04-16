0bd4628848158d836975f2cf72d72b94dfb71aeb
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-04-16 10:47:32 +0800

    Update rl_cgp.py

--- rl_cgp.py
+++ rl_cgp.py
@@ -9,11 +9,27 @@ from utils import ParentSibling, ComputingTree
 from torch.distributions import Categorical
 from CartPoleContinuous import CartPoleContinuousEnv
 
+import argparse, math, os, sys, gym
+import numpy as np
+from gym import wrappers
+from function import *
+from configuration import config
+from CartPoleContinuous import CartPoleContinuousEnv
+from torch.autograd import Variable
+import torch.autograd as autograd
+import torch.nn.utils as utils
+
+import torch.nn.functional as F
+import torch.optim as optim
+
 device = torch.device('cpu')
 
-
+env_name = 'CartPoleContinuous'
 env = CartPoleContinuousEnv()
 state = env.reset()
+env.seed(config.seed)                                                 # 随机数种子
+torch.manual_seed(config.seed)                                        # Gym、numpy、Pytorch都要设置随机数种子
+np.random.seed(config.seed)
 
 def s0():
     '''返回env状态的第0维度'''
@@ -218,41 +234,76 @@ def ApplyConstraints(tau, func_set):
     '''
     
 
+class REINFORCE:
+    def __init__(self, hidden_size, num_inputs, action_space):
+        self.action_space = action_space
+        self.model = Policy(hidden_size, num_inputs, action_space)    # 创建策略网络
+        # self.model = self.model.cuda()                              # GPU版本
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2) # 优化器
+        self.model.train()
 
-'''tau_len = []
-for i in range(1):
-    tau = policy_generator(model)
-    if tau != -1:
-        tau_len.append(len(tau))
 
-print(tau_len, len(tau_len), np.mean(tau_len))'''
+    def select_action(self, state):
+        # mu, sigma_sq = self.model(Variable(state).cuda())
+        a, b = self.model(Variable(state))
+        beta = torch.distributions.Beta(a,b)
+        sample = beta.sample()
+        action = (sample*2 - 1).item() # 定义域[-1,1]
+        log_prob = beta.log_prob(sample)
+        entropy = beta.entropy()
+        return action, log_prob, entropy
+
+    def update_parameters(self, rewards, log_probs, entropies, gamma):# 更新参数
+        R = torch.zeros(1, 1)
+        loss = 0
+        for i in reversed(range(len(rewards))):
+            R = gamma * R + rewards[i]                                # 倒序计算累计期望
+            # loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i])).cuda()).sum() - (0.0001*entropies[i].cuda()).sum()
+            loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i]))).sum() - (0.001*entropies[i]).sum()
+        loss = loss / len(rewards)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        utils.clip_grad_norm_(self.model.parameters(), 10)             # 梯度裁剪，梯度的最大L2范数=40
+        self.optimizer.step()
+
+agent = REINFORCE(config.hidden_size, env.observation_space.shape[0], env.action_space)
+
+dir = './results/ckpt_' + env_name
+if not os.path.exists(dir):    
+    os.mkdir(dir)
+
+for i_episode in range(config.num_episodes):
+    state = torch.Tensor([env.reset()])
+    entropies = []
+    log_probs = []
+    rewards = []
+    for t in range(config.num_steps): # 1个episode最长持续的timestep
+        action, log_prob, entropy = agent.select_action(state)
+        next_state, reward, done, _ = env.step(np.array([action]))
+
+        entropies.append(entropy)
+        log_probs.append(log_prob)
+        rewards.append(reward)
+        state = torch.Tensor([next_state])
+
+        if done:
+            break
+    # 1局游戏结束后开始更新参数
+    agent.update_parameters(rewards, log_probs, entropies, config.gamma)
+
+
+    if i_episode % config.ckpt_freq == 0:
+        torch.save(agent.model.state_dict(), os.path.join(dir, 'reinforce-'+str(i_episode)+'.pkl'))
+
+    print("Episode: {}, reward: {}".format(i_episode, np.sum(rewards)))
+
+env.close()
+
+
 tau = -1
 while tau==-1:
     tau, log_prob = policy_generator(model)
 R = policy_evaluator(tau, env, fs)
 print(tau)
 print(R)
-
-'''
-print(ParentSibling([],fs))
-print(ParentSibling([3],fs))
-print(ParentSibling([3,4],fs))
-print(ParentSibling([3,4,2],fs))
-print(ParentSibling([3,4,2,8],fs))
-print(ParentSibling([3,4,2,8,9],fs))
-
-
-'''
-# print(ParentSibling([3,4,2,8,9,6], fs))
-'''model =lstm(10,8,5,2)
-x, hn, cn = model(torch.rand(1,1,10), ) # torch.zeros(3,1,8), torch.zeros(3,1,8)
-print(x.shape, hn, cn)'''
-
-'''
-import time
-print(ComputingTree([3,4,2,8,9,6,10], fs))
-tick = time.time()
-for i in range(100):
-    pass; #ComputingTree([0,1,3,8,10,4,9,2,5,7,8,6,10], fs)
-print((time.time()-tick)/100*1000,'ms')
-'''
