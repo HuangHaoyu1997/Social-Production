6dfcbe67622200efbfe9bd5fd654c13cfb695e42
Haoyu Huang <446700679@qq.com>
2022-04-17 22:28:25 +0800

    update

--- rl_cgp.py
+++ rl_cgp.py
@@ -27,6 +27,7 @@ device = torch.device('cpu')
 env_name = 'CartPoleContinuous'
 env = CartPoleContinuousEnv()
 state = env.reset()
+
 env.seed(config.seed)                                                 # 随机数种子
 torch.manual_seed(config.seed)                                        # Gym、numpy、Pytorch都要设置随机数种子
 np.random.seed(config.seed)
@@ -43,7 +44,7 @@ def s2():
 def s3():
     '''返回env状态的第3维度'''
     return state[3]
-fs = [
+func_set = [
     Function(op.add, 2),        # 0
     Function(op.sub, 2),        # 1
     Function(op.mul, 2),        # 2
@@ -80,40 +81,9 @@ class lstm(nn.Module):
         return x, hn, cn
 
 
-'''
-input_dim = 10
-out_dim = 5
-pop = create_population(5,input_dim,out_dim)
-
-def cvt_bit(a,length=6):
-    # 将int a转化为定长二进制str
-    return '0'*(length-len(bin(a)[2:]))+bin(a)[2:]
-
-def gene_encoder(ind:Individual):
-    # 打印个体的节点
-    
-    gene_before = []
-    gene_after = ''
-    for node in ind.nodes:
-        # print(node.i_func, cvt_bit(node.i_func), int(cvt_bit(node.i_func),2))
-        # print(node.i_inputs[0]+10, cvt_bit(node.i_inputs[0]+10), int(cvt_bit(node.i_inputs[0]+10),2))
-        gene_after += cvt_bit(node.i_func)
-        gene_after += cvt_bit(node.i_inputs[0]+input_dim)
-        
-        gene_before.append(node.i_func)
-        gene_before.append(node.i_inputs[0]+input_dim)
-        
-        if node.i_inputs[1] is not None:
-            # print(node.i_inputs[1]+10, cvt_bit(node.i_inputs[1]+10))
-            gene_after += cvt_bit(node.i_inputs[1]+input_dim)
-            gene_before.append(node.i_inputs[1]+input_dim)
-    return gene_before, gene_after
-
-g_b, g_a = gene_encoder(pop[0])
-'''
 
 
-def policy_evaluator(tau, env, func_set=fs, episode=config.Epoch):
+def policy_evaluator(tau, env, func_set, episode=config.Epoch):
     '''
     policy evaluation
     policy is represented by a symbol sequence `tau`
@@ -134,7 +104,7 @@ def policy_evaluator(tau, env, func_set=fs, episode=config.Epoch):
         r_epi += reward
     return r_epi / episode
 
-def policy_generator(model, func_set=fs,):
+def policy_generator(model, func_set,):
     '''
     return a sequence of symbols
     '''
@@ -147,6 +117,7 @@ def policy_generator(model, func_set=fs,):
     
     counter = 1
     log_prob = 0
+    joint_entropy = 0
     hn, cn = torch.zeros(2,1,32), torch.zeros(2,1,32)
     while counter > 0:
         phi, hn, cn = model(PS, hn, cn)
@@ -160,16 +131,17 @@ def policy_generator(model, func_set=fs,):
         new_op = dist.sample()
         # print(new_op, phi_after_mask[0,0,new_op].log())
         log_prob += phi_after_mask[0,0,new_op].log()
+        joint_entropy += dist.entropy()
         tau.append(new_op.item())
         
         PS = torch.cat((P,S)).unsqueeze(0).unsqueeze(0)
         counter += func_set[new_op].arity - 1
         if counter==0: break
-        if len(tau) > config.N_COLS: return -1, 0
+        if len(tau) > config.N_COLS: return -1, 0, 0
         [iP, iS], P, S = ParentSibling(tau, func_set)
     if (func_dim-1 not in tau) and (func_dim-2 not in tau) and (func_dim-3 not in tau) and (func_dim-4 not in tau):
-        return -1, 0
-    return tau, log_prob
+        return -1, 0, 0
+    return tau, log_prob, joint_entropy
 
 def ApplyConstraints(tau, func_set):
     '''
@@ -199,50 +171,35 @@ def ApplyConstraints(tau, func_set):
         else:
             mask = torch.ones(len(func_set))
         return mask
-    '''
-    elif len(tau)>0:
-        par
-        if func_set[parent].name == 'sin':
-            mask = torch.tensor([0 if func_set[i].name=='cos' else 1 for i in range(len(func_set))])
-        elif func_set[parent].name == 'cos':
-            mask = torch.tensor([0 if func_set[i].name=='sin' else 1 for i in range(len(func_set))])
-        elif func_set[parent].name == 'log':
-            mask = torch.tensor([0 if func_set[i].name=='exp' else 1 for i in range(len(func_set))])
-        elif func_set[parent].name == 'exp':
-            mask = torch.tensor([0 if func_set[i].name=='log' else 1 for i in range(len(func_set))])
-        else:
-            mask = torch.ones(len(func_set))
-        return mask
 
-    '''
-
-model = lstm(input_size = 2*len(fs),
+model = lstm(input_size = 2*len(func_set),
                 hidden_size = 32, 
-                output_size = len(fs), 
+                output_size = len(func_set),
                 num_layer = 2
             ).to(device)
+
 class REINFORCE:
-    def __init__(self, hidden_size, num_inputs, action_space):
-        self.action_space = action_space
-        self.model = lstm(input_size = 2*len(fs),
+    def __init__(self, func_set, hidden_size):
+        '''
+        func_set: 符号库
+        
+        '''
+        self.model = lstm(input_size = 2*len(func_set),
                                 hidden_size = hidden_size, 
-                                output_size = len(fs), 
+                                output_size = len(func_set), 
                                 num_layer = 2
-                            ).to(device)
+        )
         # self.model = self.model.cuda()                              # GPU版本
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2) # 优化器
+        self.fs = func_set
         self.model.train()
 
 
-    def select_action(self, state):
-        # mu, sigma_sq = self.model(Variable(state).cuda())
-        a, b = self.model(Variable(state))
-        beta = torch.distributions.Beta(a,b)
-        sample = beta.sample()
-        action = (sample*2 - 1).item() # 定义域[-1,1]
-        log_prob = beta.log_prob(sample)
-        entropy = beta.entropy()
-        return action, log_prob, entropy
+    def symbolic_generator(self):
+        tau = -1
+        while tau == -1:
+            tau, log_prob, entropy = policy_generator(self.model, self.fs)
+        return tau, log_prob, entropy
 
     def update_parameters(self, rewards, log_probs, entropies, gamma):# 更新参数
         R = torch.zeros(1, 1)
@@ -258,7 +215,7 @@ class REINFORCE:
         utils.clip_grad_norm_(self.model.parameters(), 10)             # 梯度裁剪，梯度的最大L2范数=40
         self.optimizer.step()
 
-agent = REINFORCE(config.hidden_size, env.observation_space.shape[0], env.action_space)
+agent = REINFORCE(func_set, config.hidden_size)
 
 dir = './results/ckpt_' + env_name
 if not os.path.exists(dir):    
@@ -269,9 +226,9 @@ for i_episode in range(config.num_episodes):
     entropies = []
     log_probs = []
     rewards = []
-    for t in range(config.num_steps): # 1个episode最长持续的timestep
-        action, log_prob, entropy = agent.select_action(state)
-        next_state, reward, done, _ = env.step(np.array([action]))
+    for t in range(config.num_steps): # 1次生成10个tau,分别测试
+        tau, log_prob, entropy = agent.symbolic_generator()
+        policy_evaluator(tau, env, func_set)
 
         entropies.append(entropy)
         log_probs.append(log_prob)
@@ -295,6 +252,6 @@ env.close()
 tau = -1
 while tau==-1:
     tau, log_prob = policy_generator(model)
-R = policy_evaluator(tau, env, fs)
+R = policy_evaluator(tau, env, func_set)
 print(tau)
 print(R)
