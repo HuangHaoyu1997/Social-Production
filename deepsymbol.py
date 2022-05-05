87f79b5fcf3611c9d469b22ae70b92ba49f79fbc
Haoyu Huang <446700679@qq.com>
2022-05-05 19:37:57 +0800

    u

--- deepsymbol.py
+++ deepsymbol.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.utils as utils
 from torch.distributions import Categorical, Beta
+import torch.autograd.variable as Variable
 import numpy as np
 from function import Function
 import argparse, math, os, sys, gym, torch
@@ -15,8 +17,8 @@ import torch.optim as optim
 env_name = 'CartPoleContinuous'
 env = CartPoleContinuousEnv()
 
-env.seed(config.seed)                                                 # 随机数种子
-torch.manual_seed(config.seed)                                        # Gym、numpy、Pytorch都要设置随机数种子
+env.seed(config.seed)                   # 随机数种子
+torch.manual_seed(config.seed)          # Gym、numpy、Pytorch都要设置随机数种子
 np.random.seed(config.seed)
 
 def torchInv(x):
@@ -67,41 +69,40 @@ class Model(nn.Module):
         return x
 
 class DeepSymbol():
-    def __init__(self, env, func_set) -> None:
-        self.env = env
-        self.inpt_dim = self.env.observation_space.shape[0]
+    def __init__(self, inpt_dim, hid_dim, func_set) -> None:
+        self.inpt_dim = inpt_dim
         # s = self.env.reset()
         self.func_set = func_set
         self.dict_dim = len(func_set)
         self.model = Model(inpt_dim = self.inpt_dim,
-                            hid_dim=12, 
-                            dict_dim=self.dict_dim)
+                            hid_dim = hid_dim, 
+                            dict_dim= self.dict_dim)
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2)
         self.model.train()
     
     def select_action(self, state):
         # state = torch.tensor(state)
         idx, log_prob, entropy = self.sym_mat(state)
-        action = self.execute(state, idx)
-        action = ,action.item()
+        action = self.execute_symbol_mat(state, idx)
+        action = tanh(action.item(), alpha=0.05)
         return action, log_prob, entropy
 
     def sym_mat(self, state):
         '''
         generate the symbol matrix for observation vector
         '''
-        matrix_prob = self.model(torch.tensor(state))
+        matrix_prob = self.model(state)
         
         # find the max prob symbol for each position in matrix
         idx = torch.argmax(matrix_prob, dim=-1)
         
-        # calculate the joint log prob for all symbol selected
+        # joint log prob for all symbols selected
         log_prob = 0
         for i in range(idx.size()[0]):
             for j in range(idx.size()[1]):
                 log_prob += matrix_prob[i, j, idx[i,j]].log()
 
-        # calculate upper bound of entropy for joint symbol categorical distribution
+        # upper bound of entropy for joint symbols categorical distribution
         entropies = 0
         for i in range(idx.size()[0]):
             for j in range(idx.size()[1]):
@@ -112,27 +113,70 @@ class DeepSymbol():
         
         return idx, log_prob, entropies
     
-    def execute(self, state, idx):
+    def execute_symbol_mat(self, state, idx):
         '''
         do the symbolic calculation using state vector
         '''
-        state = torch.tensor(state)
         tmp = torch.zeros_like(idx, dtype=torch.float32)
         for i in range(idx.size()[0]):
             for j in range(idx.size()[1]):
                 arity = self.func_set[idx[i,j]].arity
                 if arity == 1:
-                    inpt = torch.tensor([state[i]])
+                    inpt = torch.tensor([state[0,i]])
                 elif arity == 2:
-                    inpt = torch.tensor([state[i], state[j]])
+                    print(state[0,i], state[0,j])
+                    inpt = torch.tensor([state[0,i], state[0,j]])
                 # print(idx[i,j], self.func_set[idx[i,j]].name, inpt)
                 tmp[i,j] = self.func_set[idx[i,j]](*inpt)
         return tmp.sum()
     
+    def update_parameters(self, rewards, log_probs, entropies, gamma):# 更新参数
+        R = torch.zeros(1, 1)
+        loss = 0
+        for i in reversed(range(len(rewards))):
+            R = gamma * R + rewards[i]                                # 倒序计算累计期望
+            # loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i])).cuda()).sum() - (0.0001*entropies[i].cuda()).sum()
+            loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i]))).sum() - (0.001*entropies[i]).sum()
+        loss = loss / len(rewards)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        utils.clip_grad_norm_(self.model.parameters(), 10)             # 梯度裁剪，梯度的最大L2范数=40
+        self.optimizer.step()
 
 
-ds = DeepSymbol(env, func_set)
-ds.select_action(env.reset())
+ds = DeepSymbol(inpt_dim=4, hid_dim=16, func_set=func_set)
+dir = './results/ckpt_deepsymbol_' + env_name
+
+if not os.path.exists(dir):    
+    os.mkdir(dir)
+
+for i_episode in range(config.num_episodes):
+    state = torch.Tensor([env.reset()])
+    entropies = []
+    log_probs = []
+    rewards = []
+    for t in range(config.num_steps): # 1个episode最长持续的timestep
+        action, log_prob, entropy = ds.select_action(state)
+        next_state, reward, done, _ = env.step(np.array([action]))
+
+        entropies.append(entropy)
+        log_probs.append(log_prob)
+        rewards.append(reward)
+        state = torch.Tensor([next_state])
+
+        if done:
+            break
+    # 1局游戏结束后开始更新参数
+    ds.update_parameters(rewards, log_probs, entropies, config.gamma)
+
+
+    if i_episode % config.ckpt_freq == 0:
+        torch.save(ds.model.state_dict(), os.path.join(dir, 'VPG-'+str(i_episode)+'.pkl'))
+
+    print("Episode: {}, reward: {}".format(i_episode, np.sum(rewards)))
+
+env.close()
 
 
 
